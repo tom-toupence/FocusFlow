5c0df8d8a6cd012fac00890c96e58a12fc6f3f49
Tom TOUPENCE <ttoupence@oxya.com>
2026-03-27 11:29:06 +0100

    Force dynamic rendering on Spotify callback page

--- app/auth/spotify/callback/page.tsx
+++ app/auth/spotify/callback/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useRef, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { handleSpotifyCallback } from "@/lib/spotify";
